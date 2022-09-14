b0cba14ebbfa7180eb1f516ac0f6a4e032feb521
Dylan Lyon <44653347+noylnalyd@users.noreply.github.com>
2022-09-14 18:01:47 -0400

    Add files via upload

---
+++ dslyon_Ex2B.f90
@@ -0,0 +1,29 @@
+module dslyon_Ex2B
+contains
+        function dslyon_Ex2B(N) result(A)
+
+        IMPLICIT NONE
+        INTEGER(4) :: N ! Dimension of z curve
+        INTEGER(4) :: i,j ! Indices
+        
+        USE :: 
+
+        READ(*,*) N ! Read in N
+        WRITE(*,*) N ! Write N
+        IF(N/=2 .and. N/=4 .and. N/=8 .and. N/=16)
+                WRITE(stderr,*) "Invalid N."
+                STOP
+        END IF
+        
+        INTEGER(4) :: A(N*N) ! Matrix to fill
+
+        DO i=1,N
+                DO j=1,N
+                        A((i-1)*N+j)=dslyon_Ex2B(i,j)
+                END DO
+        END DO
+        
+        END FUNCTION
+
+        
+END MODULE
