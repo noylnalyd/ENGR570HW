9130f57e86ca1bef20c70c4d6a132f5741e43937
Dylan Lyon <dylanscottlyon@gmail.com>
2022-09-14 19:42:41 -0400

    Made a lot of changes!

--- dslyon_Ex2B.f90
+++ dslyon_Ex2B.f90
@@ -1,18 +1,10 @@
 MODULE dslyon_Ex2B
+        IMPLICIT NONE
+        USE z_order2D
         contains
-                function dslyon_Ex2Bhelper(N) result(A)
-                INTEGER(4) :: N ! Dimension of z curve
+                INTEGER(4) function dslyon_Ex2Bhelper(N) result(res)
+                
                 INTEGER(4) :: i,j ! Indices
-                
-                
-
-                READ(*,*) N ! Read in N
-                WRITE(*,*) N ! Write N
-                IF(N/=2 .and. N/=4 .and. N/=8 .and. N/=16)
-                        WRITE(stderr,*) "Invalid N."
-                        STOP
-                END IF
-                
                 INTEGER(4) :: A(N*N) ! Matrix to fill
 
                 DO i=1,N
@@ -21,5 +13,5 @@ MODULE dslyon_Ex2B
                         END DO
                 END DO
                 
-                END FUNCTION       
+                END FUNCTION dslyon_Ex2Bhelper 
 END MODULE
---
+++ dslyon_Ex2B_main.f90
@@ -0,0 +1,11 @@
+PROGRAM dslyon_Ex2B
+    USE dslyon_Ex2B
+    IMPLICIT NONE
+    INTEGER(4) :: N ! Dimension of z curve
+    READ(*,*) N ! Read in N
+    WRITE(*,*) N ! Write N
+    IF ( (N/=2) .and. (N/=4) .and. (N/=8) .and. (N/=16) ) THEN
+        WRITE(*,*) "Invalid N."
+        STOP
+    END IF
+END PROGRAM dslyon_Ex2B
