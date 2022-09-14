db85b6170f2a24fc68707c152676b1b66390b5e7
Dylan Lyon <dylanscottlyon@gmail.com>
2022-09-14 19:20:43 -0400

    Syntax corrections.

--- dslyon_Ex2B.f90
+++ dslyon_Ex2B.f90
@@ -1,29 +1,25 @@
-module dslyon_Ex2B
-contains
-        function dslyon_Ex2B(N) result(A)
+MODULE dslyon_Ex2B
+        contains
+                function dslyon_Ex2Bhelper(N) result(A)
+                INTEGER(4) :: N ! Dimension of z curve
+                INTEGER(4) :: i,j ! Indices
+                
+                
 
-        IMPLICIT NONE
-        INTEGER(4) :: N ! Dimension of z curve
-        INTEGER(4) :: i,j ! Indices
-        
-        USE :: 
+                READ(*,*) N ! Read in N
+                WRITE(*,*) N ! Write N
+                IF(N/=2 .and. N/=4 .and. N/=8 .and. N/=16)
+                        WRITE(stderr,*) "Invalid N."
+                        STOP
+                END IF
+                
+                INTEGER(4) :: A(N*N) ! Matrix to fill
 
-        READ(*,*) N ! Read in N
-        WRITE(*,*) N ! Write N
-        IF(N/=2 .and. N/=4 .and. N/=8 .and. N/=16)
-                WRITE(stderr,*) "Invalid N."
-                STOP
-        END IF
-        
-        INTEGER(4) :: A(N*N) ! Matrix to fill
-
-        DO i=1,N
-                DO j=1,N
-                        A((i-1)*N+j)=dslyon_Ex2B(i,j)
+                DO i=1,N
+                        DO j=1,N
+                                A((i-1)*N+j)=dslyon_Ex2B(i,j)
+                        END DO
                 END DO
-        END DO
-        
-        END FUNCTION
-
-        
+                
+                END FUNCTION       
 END MODULE
