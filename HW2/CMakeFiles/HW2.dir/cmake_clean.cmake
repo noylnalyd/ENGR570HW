2c84fd95b4c1ed776bf63f8d8538f8d1bdd9a2e0
Dylan Lyon <dylanscottlyon@gmail.com>
2022-09-24 16:33:36 -0400

    Added cmakefile.

---
+++ HW2/CMakeFiles/HW2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW2.dir/HW2.f90.o"
+  "CMakeFiles/HW2.dir/mm_io.f90.o"
+  "HW2"
+  "HW2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
